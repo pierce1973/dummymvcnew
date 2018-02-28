1bd275c4fad6419a422f7eac42cfca6fe8199051
pierce1973 <pierce.treacy@gmail.com>
2018-02-28 20:14:26 +0000

    initial project

---
+++ src/main/java/com/example/restmvc/restmvcdemo/RestmvcdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.restmvc.restmvcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestmvcdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestmvcdemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/restmvc/restmvcdemo/RestmvcdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.restmvc.restmvcdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestmvcdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
